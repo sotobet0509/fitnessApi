125d09472ff584cdf1c3e7ef86f84303e4e63e64
Andres Perez <andres.perez@digitalignition.com.mx>
2021-05-07 16:27:06 -0500

    rs256 to hs256

--- src/services/token.ts
+++ src/services/token.ts
@@ -26,7 +26,7 @@ export class TokenService {
 
   private static generateAndSign(payload: object, signOptions: SignOptions): Promise<string> {
     return new Promise((resolve, reject) => {
-      jwt.sign(payload, process.env.JWT_PRIVATE_KEY, { ...signOptions, algorithm: 'RS256' }, (err, token) => {
+      jwt.sign(payload, process.env.JWT_PRIVATE_KEY, { ...signOptions, algorithm: 'HS256' }, (err, token) => {
         if (err) reject(new ErrorResponse(500, 500, 'Internal Server Error'))
         else resolve(token)
       })
@@ -36,7 +36,7 @@ export class TokenService {
   static verifyToken(token: string): Promise<any> {
     console.log("token:",token,"key:", process.env.JWT_PUBLIC_KEY )
     return new Promise((resolve, reject) => {
-      jwt.verify(token, process.env.JWT_PUBLIC_KEY, { algorithms: ['RS256'] }, (err, payload: any) => {
+      jwt.verify(token, process.env.JWT_PUBLIC_KEY, { algorithms: ['HS256'] }, (err, payload: any) => {
         
         if (err){
           console.log(err)
