a6b0eb875430ca1952e2ca326f17b2851ce9e2a5
sotobetancourte <eduardo.soto@digitalignition.com.mx>
2021-05-11 11:01:59 -0500

    'instructorClasses'

--- src/repositories/instructor.ts
+++ src/repositories/instructor.ts
@@ -146,7 +146,7 @@ export const InstructorRepository = {
         let  currentDate  = moment()
         const schedules = await createQueryBuilder(Schedule)
         .innerJoinAndSelect('Schedule.Instructor','Instructor')
-        .where('Schedule.Instructor=:instructorId',{instructorId:instructorId})
+        .where('Schedule.Instructor=:instructorId',{instructorId:data.id})
         .andWhere('Date(date)>=:cDate', { cDate: moment(currentDate).format('YYYY-MM-DD') })
         .andWhere('Time(end)>:cTime', { cTime: moment(currentDate).format("HH:mm:ss") })
         .getMany()
