512b5faef55d42fb74046a9336ecc9e9aa869ad4
sotobetancourte <eduardo.soto@digitalignition.com.mx>
2021-05-12 10:12:06 -0500

    'images'

--- src/repositories/image.ts
+++ src/repositories/image.ts
@@ -26,13 +26,13 @@ export const ImageRepository = {
     for (var i in data.images){
       const id = data.images[i]
       const image = await getRepository(Images).findOne({
-        where: {
-            id: id
-        }
-    })
-    if (!image) throw new ErrorResponse(404, 14, 'La imagen no existe '+data.images[i])
-    image[i].status = !image[i].status
-    await imageRepository.save(image)
+          where: {
+              id: id
+          }
+      })
+      if (!image) throw new ErrorResponse(404, 14, 'La imagen no existe '+data.images[i])
+      image[i].status = !image[i].status
+      await imageRepository.save(image)
     }
    
     
