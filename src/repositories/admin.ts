1a95aff5a1a512f71ccff66d739723049ddf6008
sotobet0509 <sotobetancourte@yahoo.com>
2023-06-19 23:17:29 -0600

    dateTime

--- src/repositories/admin.ts
+++ src/repositories/admin.ts
@@ -351,6 +351,7 @@ export const AdminRepository = {
         const Exercise  = new Ejercicios()
         let fecha_cita= new Date()
         fecha_cita.setHours (0, 0, 0, 0)
+        fecha_cita.setHours(-6)
         Exercise.fecha_ejercicio= fecha_cita
         Exercise.completado=false
         Exercise.Usuario=user
